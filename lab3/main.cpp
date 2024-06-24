405d34618cedb0feef05e2104ba55f9eb6e473a2
Ostanin Roman <ostanin.roma15@gmail.com>
2024-06-24 08:58:54 +0700

    Добавил вывод процентов занимаемого размера файлов относительно текущей папки

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -6,17 +6,32 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    QDir dir("..\\Test\\test4\\");
+    QDir dir("..\\Test\\test2\\");
     Counting* strategyByType = new(std::nothrow) GroupByType();
     Counter c = Counter(strategyByType);
     QHash<QString, double>H1 = c.Calc(dir);
+    double t = 0;
     for(auto h : H1.keys())
-        qDebug() << h << "----" << H1[h];
+        t += H1[h];
+    QHash<QString, double>prst1;
+    for(auto h : H1.keys()){
+        prst1.insert(h, (H1[h]*100/t));
+        qDebug() << h << "----" << H1[h] << "----" << prst1[h];
+    }
+    qDebug() << " ";
     Counting* srategyInFolders = new(std::nothrow) GroupInFolders();
     c.setStrategy(srategyInFolders);
-    qDebug() << " ";
     QHash<QString, double>H2 = c.Calc(dir);
+    t = 0;
     for(auto h : H2.keys())
-        qDebug() << h << "----" << H2[h];
+        t += H2[h];
+    QHash<QString, double>prst2;
+    for(auto h : H2.keys()){
+        if(t)
+            prst2.insert(h, (H2[h]*100/t));
+        else
+            prst2.insert(h, t);
+        qDebug() << h << "----" << H2[h] << "----" << prst2[h];
+        }
     return a.exec();
 }
